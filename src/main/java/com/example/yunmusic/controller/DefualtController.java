5889086e9eaa7e66168d92433d7804b8f0ff9c21
5201314_Zq <ya942371102@qq.com>
2022-03-31 09:18:20 +0800

    add @CrossOrigin

--- src/main/java/com/example/yunmusic/controller/DefualtController.java
+++ src/main/java/com/example/yunmusic/controller/DefualtController.java
@@ -1,11 +1,13 @@
 package com.example.yunmusic.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
+@CrossOrigin
 public class DefualtController {
     @GetMapping
     public String sayHello(){
